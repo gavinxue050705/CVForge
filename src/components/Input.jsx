848505f2d88f17706e72a01ce995de5ee3f64bc9
Gavin Xue <gavinxue050705@gmail.com>
2023-12-24 12:16:55 -0500

    Updated input label

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,23 +2,13 @@ function Input({ type, id, label, placeholder, value, onChange }) {
   return (
     <div className="input">
       <h3>{label}</h3>
-      {type === "textarea" ? (
-        <textarea
-          type={type}
-          id={id}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        ></textarea>
-      ) : (
-        <input
-          type={type}
-          id={id}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        />
-      )}
+      <input
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }

b63a3a64126f9f3379a423b42df8247ec6e15311
Gavin Xue <gavinxue050705@gmail.com>
2023-12-24 13:52:51 -0500

    Removed array in sample-data

--- public/sample-data.jsx
+++ public/sample-data.jsx
@@ -7,40 +7,22 @@ const sampleData = {
     phone: "123 456 7890",
     address: "Toronto, ON",
   },
-  education: [
-    {
-      degree: "Bachelor in Computer Science",
-      school: "University of Waterloo",
-      location: "Waterloo, CA",
-      startDate: "09/2023",
-      endDate: "present",
-    },
-    {
-      degree: "Master of Business Administration",
-      school: "MIT",
-      location: "Cambridge, US",
-      startDate: "09/2028",
-      endDate: "present",
-    },
-  ],
-  experiences: [
-    {
-      company: "Apple",
-      position: "Software Engineer",
-      location: "Cupertino, US",
-      description: "feawfwaf",
-      startDate: "01/2020",
-      endDate: "01/2021",
-    },
-    {
-      company: "Apple",
-      position: "Software Engineer",
-      location: "Cupertino, US",
-      description: "feawfwaf",
-      startDate: "01/2020",
-      endDate: "01/2021",
-    },
-  ],
+  education: {
+    degree: "Bachelor in Computer Science",
+    school: "University of Waterloo",
+    location: "Waterloo, CA",
+    startDate: "09/2023",
+    endDate: "present",
+  },
+
+  experience: {
+    company: "Apple",
+    position: "Software Engineer",
+    location: "Cupertino, US",
+    description: "feawfwaf",
+    startDate: "01/2020",
+    endDate: "01/2021",
+  },
 };
 
 export default sampleData;

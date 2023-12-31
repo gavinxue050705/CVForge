9945b4e6c40a03473a7c84e8581a5d636dd68d59
Gavin Xue <gavinxue050705@gmail.com>
2023-12-31 16:24:14 -0500

    Link directs to new tab

--- src/components/header/ButtonGithub.jsx
+++ src/components/header/ButtonGithub.jsx
@@ -3,6 +3,8 @@ function ButtonGithub() {
     <a
       className="button-github"
       href="https://github.com/gavinxue050705/CVForge"
+      target="_blank"
+      rel="noopener noreferrer"
     >
       <i className="fa-brands fa-github"></i> gavin xue
     </a>

a3822cecff9964cf7d4fd8393722478cc4f580ca
Gavin Xue <gavinxue050705@gmail.com>
2024-01-21 12:55:04 -0500

    Added projects form

---
+++ src/components/projects/ProjectsForm.jsx
@@ -0,0 +1,27 @@
+import ButtonAdd from "../ButtonAdd";
+import Projects from "./Projects";
+
+function ProjectsForm({ projects, onChange, onAdd, onRemove, onDrop }) {
+  return (
+    <div className="container">
+      <h2 onClick={onDrop} className="section-header">
+        <i className="fa-solid fa-briefcase"></i> Projects
+      </h2>
+      <div id="form-projects">
+        {projects.map((project, i) => (
+          <Projects
+            index={i}
+            onChange={onChange}
+            name={project.name}
+            technologies={project.technologies}
+            description={project.description}
+            onRemove={onRemove}
+          />
+        ))}
+        <ButtonAdd onAdd={onAdd} section="Projects" />
+      </div>
+    </div>
+  );
+}
+
+export default ProjectsForm;
